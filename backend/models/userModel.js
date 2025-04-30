c5e11c05488c310f17e58e94328de4e9fb6c2369
etowageed <etowagideon@gmail.com>
2025-04-30 23:58:56 +0100

    setup user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+
+  password: {
+    type: String, // must be hashed
+    required: [true, "Please provide a password"],
+    minLength: [8, "Password must have atleast 8 characters"],
+    select: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
