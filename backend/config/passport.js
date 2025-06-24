0c9c6f5d6bd7e66c2cfc1ffa6f6a8f958c72c2fd
etowageed <etowagideon@gmail.com>
2025-06-24 22:33:27 +0100

    added facebook OAuth

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,7 @@
 // config/passport.js
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oidc');
-// const FacebookStrategy = require('passport-facebook');
+const FacebookStrategy = require('passport-facebook');
 const User = require('../models/userModel'); // Your User model
 
 // ------------------------------------
@@ -71,46 +71,46 @@ passport.use(
 // ------------------------------------
 // Facebook Strategy (Similar logic)
 // ------------------------------------
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FACEBOOK_APP_ID,
-//       clientSecret: process.env.FACEBOOK_APP_SECRET,
-//       callbackURL: '/api/v1/auth/facebook/callback', // Ensure this matches your route
-//       profileFields: ['id', 'displayName', 'emails'], // Request necessary fields
-//     },
-//     async (accessToken, refreshToken, profile, cb) => {
-//       try {
-//         let user = await User.findOne({ facebookId: profile.id });
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: '/api/v1/auth/facebook/callback', // Ensure this matches your route
+      profileFields: ['id', 'displayName', 'emails'], // Request necessary fields
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        let user = await User.findOne({ facebookId: profile.id });
 
-//         if (user) {
-//           return cb(null, user);
-//         } else {
-//           if (profile.emails && profile.emails.length > 0) {
-//             user = await User.findOne({ email: profile.emails[0].value });
-//             if (user) {
-//               user.facebookId = profile.id;
-//               await user.save();
-//               return cb(null, user);
-//             }
-//           }
-//           const newUser = new User({
-//             facebookId: profile.id,
-//             name: profile.displayName,
-//             email:
-//               profile.emails && profile.emails.length > 0
-//                 ? profile.emails[0].value
-//                 : null,
-//           });
-//           await newUser.save();
-//           return cb(null, newUser);
-//         }
-//       } catch (err) {
-//         return cb(err, null);
-//       }
-//     }
-//   )
-// );
+        if (user) {
+          return cb(null, user);
+        } else {
+          if (profile.emails && profile.emails.length > 0) {
+            user = await User.findOne({ email: profile.emails[0].value });
+            if (user) {
+              user.facebookId = profile.id;
+              await user.save();
+              return cb(null, user);
+            }
+          }
+          const newUser = new User({
+            facebookId: profile.id,
+            name: profile.displayName,
+            email:
+              profile.emails && profile.emails.length > 0
+                ? profile.emails[0].value
+                : null,
+          });
+          await newUser.save();
+          return cb(null, newUser);
+        }
+      } catch (err) {
+        return cb(err, null);
+      }
+    }
+  )
+);
 
 // TODO:facebook Oauth
 // Export passport to be used in your app.js
